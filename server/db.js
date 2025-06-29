7bd248d46e2cab82ebb553d068f6aa845b1680e4
sherosomu <168347705+sherosomu@users.noreply.github.com>
2025-06-29 22:02:00 +0530

    Update db.js

--- server/db.js
+++ server/db.js
@@ -1,16 +1,14 @@
-import pg from "pg";
+import { Client } from "pg";
 import dotenv from "dotenv";
 dotenv.config();
 
-const { Client } = pkg;
-
 const db = new Client({
-    const db = new Client({
-    connectionString: process.env.DATABASE_URL,
-    ssl: {
-        rejectUnauthorized: false, // Required by Render
-    },
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
+
 db.connect();
 
 export default db;
