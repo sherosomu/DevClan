cede6ffa6c09bf329f728f62a1db56b54f404ca3
sherosomu <168347705+sherosomu@users.noreply.github.com>
2025-06-29 23:26:47 +0530

    Update api.js

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 const api = axios.create({
-    baseURL: import.meta.env.VITE_API_BASE_URL,
+    baseURL: "https://devclan-backend.onrender.com",
     withCredentials: true,
 });
 
